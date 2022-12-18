e9aa35b922c04f7794f8ba40b532664cd0b886e1
Rosen Santev <r.ssantev@gmail.com>
2022-12-18 15:56:48 +0200

    add script for funds transfer

---
+++ scripts/fund_node.ts
@@ -0,0 +1,33 @@
+import { ethers } from "hardhat";
+
+const STAKING_CONTRACT_ADDRESS = process.env.STAKING_CONTRACT_ADDRESS ?? "";
+const STAKE_AMOUNT = ethers.utils.parseEther("1");
+
+async function main() {
+  const [account, mainNode] = await ethers.getSigners();
+
+  console.log(
+    "Fund node",
+    "from",
+    account.address
+  );
+  console.log("Account balance:", (await account.getBalance()).toString());
+
+  const tx = await mainNode.sendTransaction({
+    to: account.address,
+    value: STAKE_AMOUNT,
+    nonce: await mainNode.getTransactionCount(),
+    gasLimit: "0x100000",
+    gasPrice: await mainNode.getGasPrice(),
+  });
+  const receipt = await tx.wait();
+
+  console.log("receipt", receipt);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
